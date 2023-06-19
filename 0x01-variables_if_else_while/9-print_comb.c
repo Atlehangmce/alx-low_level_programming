c33cd627a7b909659fe3e96f47534182f5e3b8b7
Atlehangmce <atlehangmcephe26@gmail.com>
2023-06-19 00:55:02 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int no;
+
+	for (no=0; no<=9; no++)
+	{
+		putchar((no % 10) + '0');
+
+		if (no==9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+
+}
