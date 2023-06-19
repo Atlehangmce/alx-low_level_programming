6f90f69212b319dbc633a48e48dad75cb9fb74e0
Atlehangmce <atlehangmcephe26@gmail.com>
2023-06-19 02:03:17 -0700

    nventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int no1;
+	int no2;
+	for (no1 = 0; no1 < 9; no1++)
+	{
+		for (no2 = no1 + 1; no2 < 9; no2++)
+
+		putchar((no1 % 10) + '0');
+		putchar((no2 % 10) + '0');
+		
+		if (no1 == 8 && no2 == 9)
+		continue;
+		putchar(',');
+		putchar(' ');
+	}	
+		putchar('\n');
+	return(0);
+}
